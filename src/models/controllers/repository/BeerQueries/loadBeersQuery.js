8ec0d94370b6155d0cc51e5fae5342d3d19ea831
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2019-09-11 23:15:43 -0300

    monad ok

--- src/models/controllers/repository/BeerQueries/loadBeersQuery.js
+++ src/models/controllers/repository/BeerQueries/loadBeersQuery.js
@@ -2,6 +2,6 @@
 import type { HttpRequestOptions } from "./../../../types/HttpRequestOptions";
 
 export default (page: number): HttpRequestOptions => ({
-  url: `https://api.punkapi.com/v2/beers?page=${page}&per_page=80`,
+  url: `https://api.punkapi.com/v2/beers?page=${page}&per_page=3`,
   method: "GET"
 });
--- src/store/selectors/selectFilteredBeers.js
+++ src/store/selectors/selectFilteredBeers.js
@@ -1,1 +1,7 @@
-export default state => state.beers.filteredList;
+import Maybe from "../../utils/monads";
+
+export default state =>
+  Maybe.of(state)
+    .map(data => data.beers)
+    .map(data => data.filteredList)
+    .get([]);
